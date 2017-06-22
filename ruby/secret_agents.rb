a65cd8bf1cfc97ab1dc578c58e1aa590e65326df
klorealf <klorealf@gmail.com>
2017-06-22 16:57:04 -0400

    add code for password and corresponding alphabet value

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,13 +1,20 @@
-
-p "password".next
-
+def encryption
 index = 0
 secret_password = "password"
 input = ""
-
+puts "What's the password?"
 while index < secret_password.length
-  puts "What's the password?"
   p secret_password[index]
   index += 1
-  input = gets.chomp
+ end
 end
+
+
+s=("a".."z").to_a.join!
+secret_password="password"
+secret_password.gsub(/[a-z]/){|c|s.index(c)-1} 
+
+
+str="abcdefghijklmnopqrstuvwxyz"
+str[3]
+
