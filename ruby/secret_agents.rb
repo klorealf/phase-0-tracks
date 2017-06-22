2d4fd4da3decf393b27514b75b167b6fa7d50a74
klorealf <klorealf@gmail.com>
2017-06-22 12:16:41 -0400

    add password and while condition

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,11 @@
+p "password".next
+
+index = 0
+secret_password = "password"
+input = ""
+
+while index < secret_password.length
+  p secret_password[index]
+  index += 1
+  input = gets.chomp
+end
