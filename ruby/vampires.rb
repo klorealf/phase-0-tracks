262b291ab25320e8b000e396fb75fb2cb5912916
klorealf <klorealf@gmail.com>
2017-06-25 19:44:55 -0400

    add allergy question loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,59 +1,59 @@
-p "How many employees will be processed?"
-number_employees = gets.chomp.to_i
+  p "How many employees will be processed?"
+  number_employees = gets.chomp.to_i
 
 loop_counter = 0
 
 loop do 
-p "Employee number: #{loop_counter+1}"
-p "What is your name?"
-	name = gets.chomp
+  p "Employee number: #{loop_counter+1}"
+  p "What is your name?"
+  	name = gets.chomp
+  
+  if name == "Drake Cula"
+  	vampire_name = true
+  	elsif name == "Tu Fang"
+  	vampire_name = true 
+   else
+   	vampire_name = false
+  end
+  
+  p "How old are you?"
+  	age = gets.chomp.to_i
+  
+  p "What year were you born?"
+  	year = gets.chomp.to_i
+  
+  if age <= 150 && year >= 1867
+  	age_right = true
+   elsif age > 150
+  	age_right = false
+   elsif year < 1867
+  	age_right = false
+  	else
+  	  p "nil"
+  end 
 
-if name == "Drake Cula"
-	vampire_name = true
-	elsif name == "Tu Fang"
-	vampire_name = true 
- else
- 	vampire_name = false
-end
 
-p "How old are you?"
-	age = gets.chomp.to_i
-
-p "What year were you born?"
-	year = gets.chomp.to_i
-
-if age <= 150 && year >= 1867
-	age_right = true
- elsif age > 150
-	age_right = false
- elsif year < 1867
-	age_right = false
-	else
-	  p "nil"
-end 
-
-
-p "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
-garlic = gets.chomp.downcase 
-
-if garlic == "yes".downcase
-  vampire_like_garlic = true
- elsif garlic == "no".downcase
-  vampire_like_garlic = false
- else
-  p "nil"  
-end
-
-p "Would you like to enroll in the company's health insurance? (Yes/No)"
-insurance = gets.chomp.downcase
-
-if insurance == "yes".downcase
-  vampire_want_insurance = true
- elsif insurance == "no".downcase
-  vampire_want_insurance = false
- else
-  puts nil
-end
+  p "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
+  garlic = gets.chomp.downcase 
+  
+  if garlic == "yes".downcase
+    vampire_like_garlic = true
+   elsif garlic == "no".downcase
+    vampire_like_garlic = false
+   else
+    p "nil"  
+  end
+  
+  p "Would you like to enroll in the company's health insurance? (Yes/No)"
+  insurance = gets.chomp.downcase
+  
+  if insurance == "yes".downcase
+    vampire_want_insurance = true
+   elsif insurance == "no".downcase
+    vampire_want_insurance = false
+   else
+    puts nil
+  end
 
 
  if age_right && vampire_like_garlic || vampire_want_insurance 
@@ -68,10 +68,28 @@ end
   p "Results inconclusive"
   break
  end
- loop_counter += 1
-end
+ 
 
- 	
+loop_allergy_counter = 0
+  puts "Do you have any allergies?List one at a time. Type 'done' when finised"
+loop do 
+   puts "Allergy: #{loop_allergy_counter+1}"
+   allergy = gets.chomp.downcase
+   puts "Allergy type: #{allergy}"
+   if allergy == "done"
+     puts  "Thank you"
+     break
+     elsif allergy == "sunshine"
+     puts "Probably a vampire."
+     break
+    else
+   end
+ loop_allergy_counter += 1
+end
+ 
+ 
+ loop_counter += 1
+end 
  	
  	
  	
