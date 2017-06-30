e1321b6e51dc90605f7bbab5f96f056957f49d18
klorealf <klorealf@gmail.com>
2017-06-29 21:39:42 -0400

    replace ??? with method

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,39 +1,39 @@
-# Replace in the "<???>" with the appropriate method (and arguments, if any).
-# Uncomment the calls to catch these methods red-handed.
+ Replace in the "<???>" with the appropriate method (and arguments, if any).
+ Uncomment the calls to catch these methods red-handed.
 
-# When there's more than one suspect who could have
-# committed the crime, add additional calls to prove it.
+ When there's more than one suspect who could have
+ committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+ "iNvEsTiGaTiOn".swapcase
+ => “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+ "zom".insert(2, 'o')
+ => “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".center(20)
+ => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+ => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"the usual".concat(" suspects")
+ => "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+ " suspects".prepend("the usual")
+ => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+ => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+"The mystery of the missing first letter".reverse.chop.reverse
+ => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"Elementary,    my   dear        Watson!".squeeze(" ")
+=> "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+"z".each_byte {|c| puts c, ' ' }
+=> 122 
+(What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count('a')
+ => 4
