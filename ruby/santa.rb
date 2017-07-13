bb2265a117e1cb061ab7a8bbaf977149dd242bd8
klorealf <klorealf@gmail.com>
2017-07-13 14:55:56 -0400

    create 3 instances: speak, eat_milk_and_cookies, and initialze

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,2 +1,15 @@
 class Santa
+
+	def speak(str)
+		p "Ho, Ho Ho! Happy Holidays!"
+	end
+
+	def eat_milk_and_cookies(type)
+		p "That was a good #{type} cookie!"
+	end
+
+	def initialize
+		p "Initializing Santa instance..."
+	end
+
 end
