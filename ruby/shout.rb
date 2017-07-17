fac057cc01b2e8964686057bc84a13dd8e6105bf
klorealf <klorealf@gmail.com>
2017-07-17 00:00:33 -0400

    make mixin version of shout module with two classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,45 @@
 # module Shout
 #   # we'll put some methods here soon, but this code is fine for now!
 #end 
+
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words + "!!!" + " =) "
+# 	end
+# end
+
+# p Shout.yell_angrily("Go away")
+# p Shout.yell_happily("I love you")
+
+
+
+module Shout
+	def yell_angrily(words)
+		puts words.upcase + "!!!" +  ">=("
+	end
+
+	def yell_happily(words)
+		puts words + "!!!" + " =)"
+	end
+end
+
+
+class Rollercoaster
+	include Shout
+end
+
+
+class Parent
+	include Shout
+end
+
+rollercoaster = Rollercoaster.new
+rollercoaster.yell_angrily("Ahh")
+rollercoaster.yell_happily("Yay")
+
+parent = Parent.new
+parent.yell_angrily("Clean your room")
+parent.yell_happily("I am proud of you")
