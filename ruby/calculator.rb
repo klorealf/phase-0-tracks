b67783cfd9618067946595376c692d0facbb24e8
klorealf <klorealf@gmail.com>
2017-07-20 08:55:27 -0400

    change made for iteration

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,34 +1,20 @@
 def calculate(a, operator, b)
    if operator == '+'
-     return a + b
    elsif operator == '-'
-     return a - b
+    return a - b
    elsif operator == '*'
-     return a * b
+    return a * b
    elsif operator == '/'
-     return a / b
+    return a / b
    end
 end
- 
-def interface(a, operator, b)
+
+#p calculate(1, '+', 5)
+#p calculate(1, '-', 5)
+#p calculate(1, '*', 7)
+#p calculate(10, '/', 5)
    puts "What calulation would you like to perform?"
    calculation = gets.chomp
    calculation = calculation.split(//)
    if calculation.include?(" ")
-     calculation = calculation.delete(" ")
-   end
-   calculate(calculation[0].to_i, calculation[1], calculation[2].to_i)
-end
-
-=begin
-calculate(4, '+', 5)
-calculate(4, '-', 5)
-calculate(4, '*', 5)
-calculate(4, '/', 5)
-=end
-
-<<<<<<< HEAD
-p interface(6, '*', 8)
-=======
-p interface(5, '*', 5)
->>>>>>> 4320f06749df599773c6090d68fda8c440f53aaa
+      # did you know the .delete will only delete the value if it is there?
