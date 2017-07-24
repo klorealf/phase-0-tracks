da62bd3e242314dd861126658f337a1271e12213
klorealf <klorealf@gmail.com>
2017-07-24 01:28:46 -0400

    add function for longest array

--- js/algos.js
+++ js/algos.js
@@ -6,3 +6,17 @@ Set a temporary longest variable to the first word in the array
 repeat until all words have been checked
  return the temporary longest
 =end
+
+function findLongest(ary){
+	var length = 0;
+	
+	for (var i = 0; i < ary.length; i++){
+		if (ary[i].length > length){
+			var length = ary[i].length;
+			var longestWord = ary[i];
+		}
+	}
+	return longestWord;
+}
+
+console.log(findLongest(["small string", "longest craziest string", "medium string", "string"]));
