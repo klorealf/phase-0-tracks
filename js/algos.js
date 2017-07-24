414a7525cbb92ac33ad128d42f06f624f834d775
klorealf <klorealf@gmail.com>
2017-07-24 01:26:14 -0400

    add pseudocode for code

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+=begin
+ Longest Word
+Set a temporary longest variable to the first word in the array
+ Using .length on each string, compare each string to the temporary longest
+ if the next word is longer, set the next word as temporary longest
+repeat until all words have been checked
+ return the temporary longest
+=end
