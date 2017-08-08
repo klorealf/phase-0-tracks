c32b3f1b2d3def04e9e0a44d0bd2b81ee35ca62a
klorealf <klorealf@gmail.com>
2017-08-07 20:02:14 -0400

    refactor methods for predicted_deaths and speed_of_spread.

--- ruby/gps6_goldenbears/my_solution.rb
+++ ruby/gps6_goldenbears/my_solution.rb
@@ -15,30 +15,32 @@ class VirusPredictor
     @population = population
     @population_density = population_density
   end
-private
+
 #calls the 2 methods. returns the value for the last method 
   def virus_effects
     predicted_deaths
     speed_of_spread
   end
 
-  
+ private 
 
 #calculates the number of deaths based on population returning the number of deaths rounded down. It prints a statement about state's number of deaths. 
   def predicted_deaths
     # predicted deaths is solely based on population density
     #.floor rounds down to an integer
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      scale_factor = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      scale_factor = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      scale_factor = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      scale_factor = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      scale_factor = 0.05
     end
+
+    number_of_deaths = (@population * scale_factor).floor 
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
@@ -51,15 +53,15 @@ private
     speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
