93cc9c0ba8f4c7740384dace9b313671b1c12bfa
klorealf <klorealf@gmail.com>
2017-08-07 20:37:36 -0400

    add function and parameter

--- js/explore.js
+++ js/explore.js
@@ -6,15 +6,4 @@
 'use strict';
 
 function reverse(string) {
-  let newString = '';
-  for(let i = string.length - 1; i >= 0; i--) {
-    newString += string[i];
-  }
-  return newString;
-}
-
-const reversedString = reverse('This string is reversed');
-
-if(1 + 1 === 2) {
-  console.log(reversedString);
 }
