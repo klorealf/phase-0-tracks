fdd05b0a13eabfa4a637a43b8534d6a7c8746422
klorealf <klorealf@gmail.com>
2017-08-20 23:15:10 -0400

    create string delimiters

--- databases/spa.rb
+++ databases/spa.rb
@@ -6,3 +6,21 @@ require 'faker'
 @db = SQLite3::Database.new("spa.db")
 @db.results_as_hash = true
 
+create_customers_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS customers (
+		id INTEGER PRIMARY KEY,
+		customer_name VARCHAR (255),
+		customer_age INT,
+		service_wanted VARCHAR (255)
+	)
+
+SQL
+
+create_services_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS servies (
+ 		id INTEGER PRIMARY KEY,
+ 		service_available VARCHAR (255),
+ 		cost INT
+	)
+
+	
